13c35170f1162770b0f39eb1de51a9b001952857
Fabian Rittmeier <fr@gerinet.de>
2024-09-10 15:12:12 +0200

    inject client, increase to 10 mb body size

--- Sources/Webservice/Webservice.swift
+++ Sources/Webservice/Webservice.swift
@@ -2,16 +2,20 @@ import Foundation
 import AsyncHTTPClient
 
 public struct Webservice {
-    public init() { }
+    private let httpClient: HTTPClient
+    
+    public init(httpClient: HTTPClient = .shared) {
+        self.httpClient = httpClient
+    }
     
     public func request<Output: Response>(endpoint: Endpoint<Output>) async throws -> Output {
         let request = try endpoint.asRequest()
-        let response = try await HTTPClient.shared.execute(request, timeout: .seconds(Int64(endpoint.timeout)))
+        let response = try await httpClient.execute(request, timeout: .seconds(Int64(endpoint.timeout)))
         
         if response.status == .ok {
             let decoder = Output.decoder
-            let fiveMegaBytes = 1024 * 1024 * 5
-            let body = try await response.body.collect(upTo: fiveMegaBytes)
+            let tenMegaBytes = 1024 * 1024 * 10
+            let body = try await response.body.collect(upTo: tenMegaBytes)
             return try decoder.decode(Output.self, from: body)
         } else {
             throw WebserviceError.invalidStatusCode(Int(response.status.code))
